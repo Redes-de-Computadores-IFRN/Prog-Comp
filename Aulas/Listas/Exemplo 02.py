8ff2c565ed5116b13d9f90cdea0e10fb82d0420e
Liedson G. V. de Aquino <l.g.v.a.1700@gmail.com>
2025-01-17 22:52:19 +0000

    Primeiros exemplos de Lista

---
+++ Aulas/Listas/Exemplo 02.py
@@ -0,0 +1,9 @@
+'''
+   EXEMPLO 02:
+   A partir do código do Exemplo 01, faça as seguintes alterações:
+
+   a) Ao sair do laço, informe quantos nomes há na lista;
+   b) Caso o nome digitado após sair do laço exista na lista, 
+      informe em qual posição ele está.
+'''
+
---
+++ Aulas/Listas/Exemplo 03.py
@@ -0,0 +1,8 @@
+'''
+   EXEMPLO 03:
+   A partir do código do Exemplo 01, faça a seguinte alteração:
+
+   a) Caso a lista possua nomes exiba cada nome em uma linha.
+      A exibição deve ser em ordem alfabética.
+'''
+
